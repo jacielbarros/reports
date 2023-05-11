54069aee96b00e840b1163a77a12df5b367d2c05
jacielbarros <jbarros86@gmail.com>
2023-05-11 20:20:31 -0300

    Create criar-array-agrupado.sql
    Essa query pode auxiliar na importação da tabela chapas, criando um array agrupado.

---
+++ criar-array-agrupado.sql
@@ -0,0 +1,17 @@
+-- Criar array agrupado 
+-- Usar para importar tabela chapa - eleicaonet
+SELECT 
+c.codigo as diretorio,
+c."Nome da Chapa",
+json_agg(
+    json_build_object(
+        'index',c.ordem,
+        'nome',c."Nome do candidato",
+        'cargo',c.cargo,
+        'foto','https://eleicaonet-public.s3.sa-east-1.amazonaws.com/sbc/chapas/658794f654c906bd0988af7533348838.png'
+)) as participantes,
+ROW_NUMBER() OVER (PARTITION BY c.codigo) as ordem, --FALTA USAR A ORDEM AQUI, PORQUE PODE NÃO BATER
+ROW_NUMBER() OVER (PARTITION BY c.codigo) as numero
+into conferir_tratada
+from conferir c
+group by c.codigo, c."Nome da Chapa"
